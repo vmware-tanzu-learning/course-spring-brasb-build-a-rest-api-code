43426f2fa6b3535fb51de9362d56324d82b26153
Carlton Schuyler <cschuyler@vmware.com>
2022-10-26 11:23:51 -0600

    <initializr-end> Initial codebase generated from Spring Initializr

---
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
