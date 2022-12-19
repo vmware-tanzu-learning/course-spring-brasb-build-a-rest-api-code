cb5ae8c0b1cfba6d8855e4c1c4112573355e842e
Carlton Schuyler <cschuyler@vmware.com>
2022-12-19 12:49:12 -0700

    <get-list-start> Changes to codebase for beginning of Get List lesson:
    - Add @DirtiesContext to tests
    - Add imports to Controller class (for convenience when code is added during the lab)
    - Add more Cash Card fixtures to data.sql
    - Refactor CashCardJsonTest to include all Cash Card fixtures
    - Rename expected.json to single.json, and add list.json

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,6 +1,7 @@
 package example.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
