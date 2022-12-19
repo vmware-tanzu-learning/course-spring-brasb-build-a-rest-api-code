b1bbc2d3ebe34bfc1bdac6ef8e51b25f43bc8718
Carlton Schuyler <cschuyler@vmware.com>
2022-12-19 12:49:12 -0700

    <get-list-start> Changes to codebase for beginning of Get List lesson:
    - Add @DirtiesContext to tests
    - Add imports to Controller class (for convenience when code is added during the lab)
    - Add more Cash Card fixtures to data.sql
    - Refactor CashCardJsonTest to include all Cash Card fixtures
    - Rename expected.json to single.json, and add list.json

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,6 +1,8 @@
 package example.cashcard;
 
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -16,10 +18,23 @@ class CashCardJsonTest {
     @Autowired
     private JacksonTester<CashCard> json;
 
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.array(
+                new CashCard(99L, 123.45),
+                new CashCard(100L, 100.00),
+                new CashCard(101L, 150.00));
+    }
+
     @Test
     void cashCardSerializationTest() throws IOException {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        CashCard cashCard = cashCards[0];
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("single.json");
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
                 .isEqualTo(99);
