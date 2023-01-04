3d51cbf40c92a310c2c3594462f831de077e82d0
Bill Kable <bkable@gmail.com>
2023-01-04 15:22:31 -0600

    WIP: update to Spring Security 5.8.0 for requestMatcher

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -3,7 +3,6 @@ package example.cashcard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,7 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .antMatchers("/cashcards/**")
+                .requestMatchers("/cashcards/**")
                 .hasRole("CARD-OWNER")
                 .and()
                 .csrf().disable()
