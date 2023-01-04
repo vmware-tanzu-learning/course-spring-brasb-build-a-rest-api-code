2a96c89ac55dcdb9858d6cd7ab457e3dd9176859
Bill Kable <bkable@gmail.com>
2023-01-04 15:40:00 -0600

    WIP: update to Spring Security 6, Boot 3

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,7 +1,9 @@
 package example.cashcard;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CashCardRepository extends PagingAndSortingRepository<CashCard, Long> {
+public interface CashCardRepository extends PagingAndSortingRepository<CashCard, Long>,
+        CrudRepository<CashCard, Long> {
     CashCard findByIdAndUsername(Long id, String username);
 }
--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -1,6 +1,7 @@
 package example.cashcard;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
