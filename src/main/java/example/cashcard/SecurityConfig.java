c2b4ab2a05e49779e8b86d99d70b36ab9b34eaa3
Joe Moore <joe.moore@gmail.com>
2023-01-10 14:35:07 -0700

    <simple-security-start> Set up simple security lab
    - Add username to .sql files
    - Add username to .json files
    - Add username to CashCard and update references
    - Update all tests with username reference as needed
    - Add initial, commented-out SecurityConfig class

---
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -0,0 +1,24 @@
+package example.cashcard;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+//
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+}
