f58db32f9977a17a7c1048e56c828e1f3fadddff
Joe Moore <joe.moore@gmail.com>
2023-01-10 14:35:07 -0700

    <simple-security-start> Set up simple security lab
    - Add username to .sql files
    - Add username to .json files
    - Add username to CashCard and update references
    - Update all tests with username reference as needed
    - Add initial, commented-out SecurityConfig class

--- src/main/java/example/cashcard/CashCard.java
+++ src/main/java/example/cashcard/CashCard.java
@@ -2,5 +2,5 @@ package example.cashcard;
 
 import org.springframework.data.annotation.Id;
 
-public record CashCard(@Id Long id, Double amount) {
+public record CashCard(@Id Long id, Double amount, String owner) {
 }
