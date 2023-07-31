121ac800f53b77d270c5f6b10f1ef42d76817710
Daniel Frey <dmfrey@gmail.com>
2023-07-31 15:39:49 -0400

    add reference to method deprecated in Spring Boot 2.4, logging changes
    This method is guaranteed to have been removed by the time the app is upgraded to Spring Boot 2.7

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -1,18 +1,54 @@
 package example.cashcard;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
 
 @SpringBootApplication
 public class CashCardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CashCardApplication.class, args);
+
+		MyApplicationListener myApplicationListener = new MyApplicationListener();
+		SpringApplication springApplication = new SpringApplication(CashCardApplication.class);
+		springApplication.addListeners(myApplicationListener);
+
+		myApplicationListener.onApplicationEvent(new MyApplicationStartingEvent(springApplication, args));
+
+		springApplication.run(args);
+
+	}
+
+}
+
+class MyApplicationListener implements ApplicationListener<MyApplicationStartingEvent> {
+
+	private static final Logger log = LoggerFactory.getLogger(MyApplicationListener.class);
+
+	@Override
+	public void onApplicationEvent(MyApplicationStartingEvent event) {
+		log.info("MyApplicationStartingEvent fired!");
+	}
+
+}
+
+class MyApplicationStartingEvent extends ApplicationStartingEvent {
+
+	private static final Logger log = LoggerFactory.getLogger(MyApplicationStartingEvent.class);
+
+	@Deprecated
+	public MyApplicationStartingEvent(SpringApplication application, String[] args) {
+		super(application, args);
+		log.warn("My parent is deprecated!");
+	}
+
+	public MyApplicationStartingEvent(ConfigurableBootstrapContext bootstrapContext, SpringApplication application, String[] args) {
+		super(bootstrapContext, application, args);
+		log.info("My parent is Ok!");
 	}
 
 }
