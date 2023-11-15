0b854c323a457fc24d76e7f71767dc82faab3165
Carlton Schuyler <cschuyler@vmware.com>
2023-11-15 11:34:20 -0700

    <get-list-end> Completed tests and implementation for GET Multiple Cash Cards lesson

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -3,5 +3,5 @@ package example.cashcard;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-interface CashCardRepository extends CrudRepository<CashCard, Long> {
+interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
 }
