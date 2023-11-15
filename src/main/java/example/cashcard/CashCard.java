b634d34c4966cd3a83ac929a62fb1795cebf71dd
Carlton Schuyler <cschuyler@vmware.com>
2023-11-15 11:30:17 -0700

    <simple-security-start> Set up simple security lab
    - Add username to .sql files
    - Add username to .json files
    - Add username to CashCard and update references
    - Update all tests with username reference as needed
    - Add initial, commented-out SecurityConfig class

--- src/main/java/example/cashcard/CashCard.java
+++ src/main/java/example/cashcard/CashCard.java
@@ -2,5 +2,5 @@ package example.cashcard;
 
 import org.springframework.data.annotation.Id;
 
-record CashCard(@Id Long id, Double amount) {
+record CashCard(@Id Long id, Double amount, String owner) {
 }
---
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -0,0 +1,25 @@
+package example.cashcard;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+
+class SecurityConfig {
+//
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http.build();
+//    }
+//
+//    @Bean
+//    PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+}
