5eed49511c307e2717950cd08a33d775d3e44678
Carlton Schuyler <cschuyler@vmware.com>
2023-11-15 11:36:51 -0700

    <simple-security-end> <implement-put-start> Completed tests and implementation for Simple Spring Security lab

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,7 +1,12 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+    CashCard findByIdAndOwner(Long id, String owner);
+
+    Page<CashCard> findByOwner(String owner, PageRequest pageRequest);
 }
